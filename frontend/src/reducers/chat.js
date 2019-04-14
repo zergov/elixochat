3d207d91bdd3f96f5918223c2bb726425a232e34
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-04-14 03:45:11 -0400

    redirect to lobby if we leave current room

--- frontend/src/reducers/chat.js
+++ frontend/src/reducers/chat.js
@@ -6,6 +6,9 @@ export default (state = initialState, action) => {
   switch(action.type) {
     case "SET_CURRENT_ROOM":
       return {...state, currentRoom: action.room}
+    case "LEAVE_ROOM": {
+      return action.room === state.currentRoom ? {...state, currentRoom: 'lobby'} : state
+    }
     case "JOINED_ROOM":
       return {...state, currentRoom: action.payload.room_name}
     default:
