f4beab386ea67dbc5ee9238567a3c922330ce7c0
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-04-13 22:57:02 -0400

    feedback when connecting to the lobby

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Socket } from 'phoenix'
 import './App.css';
 
 export default props => {
   const [messages, setMessages] = useState([])
   const [users, setUsers] = useState(["zergov", "goyette"])
+
+  // connect to the chat server when mounting the app
+  useEffect(() => {
+    const socket = new Socket('ws://localhost:4000/socket')
+    socket.connect()
+
+    // join the lobby
+    const lobby = socket.channel('room:lobby')
+    lobby.join()
+      .receive("ok", () => addMessage("Connected to lobby.") )
+      .receive("error", ({reason}) => console.log("failed join", reason) )
+      .receive("timeout", () => console.log("Networking issue. Still waiting..."))
+  }, [])
 
   function addMessage(message) {
     setMessages([...messages, message])
