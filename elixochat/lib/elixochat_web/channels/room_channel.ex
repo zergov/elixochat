e1a62d2652cb4e0e1c16b0fb3a65ca65736ddc1e
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-04-14 02:30:27 -0400

    confirm room join

--- elixochat/lib/elixochat_web/channels/room_channel.ex
+++ elixochat/lib/elixochat_web/channels/room_channel.ex
@@ -1,8 +1,14 @@
 defmodule ElixochatWeb.RoomChannel do
   use Phoenix.Channel
 
-  def join("room:lobby", _message, socket) do
+  def join("room:" <> room_name, _message, socket) do
+    send(self(), {:after_join, room_name})
     {:ok, socket}
+  end
+
+  def handle_info({:after_join, room_name}, socket) do
+    push(socket, "JOINED_ROOM", %{room_name: room_name})
+    {:noreply, socket}
   end
 
   def handle_in("SEND_MESSAGE", %{"message" => message}, socket) do
