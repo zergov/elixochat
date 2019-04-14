1922dbc2044515667439924303a34d8f24ef46ba
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-04-14 01:14:58 -0400

    handle SEND_MESSAGE actions

--- elixochat/lib/elixochat_web/channels/room_channel.ex
+++ elixochat/lib/elixochat_web/channels/room_channel.ex
@@ -4,4 +4,9 @@ defmodule ElixochatWeb.RoomChannel do
   def join("room:lobby", _message, socket) do
     {:ok, socket}
   end
+
+  def handle_in("SEND_MESSAGE", %{"message" => message}, socket) do
+    broadcast!(socket, "NEW_MESSAGE", %{message: message})
+    {:noreply, socket}
+  end
 end
