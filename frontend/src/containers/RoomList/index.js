6a01d1f4cea2e8e09669d662cd26403bfdb8dda2
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-04-14 02:53:00 -0400

    room list uses rooms reducer

--- frontend/src/containers/RoomList/index.js
+++ frontend/src/containers/RoomList/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default props => {
+function RoomList({ rooms }) {
   return (
     <section className="room-list">
       <div className="room-selection">
         <input type="text" placeholder="join room"/>
       </div>
       <ul>
-        { ["lobby"].map((room, i) => <li key={i}>{room}</li>) }
+        { rooms.map((room, i) => <li key={i}>{room}</li>) }
       </ul>
     </section>
   )
 }
+
+function mapStateToProps(state) {
+  return {
+    rooms: Object.keys(state.rooms)
+  }
+}
+
+export default connect(mapStateToProps, null)(RoomList)
