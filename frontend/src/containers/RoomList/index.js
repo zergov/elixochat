3a6c5360fde1d4ed9521e2907eb3d9182d2726b3
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-04-14 02:22:24 -0400

    layout

--- frontend/src/containers/RoomList/index.js
+++ frontend/src/containers/RoomList/index.js
@@ -4,7 +4,7 @@ export default props => {
   return (
     <section className="room-list">
       <div className="room-selection">
-        <input type="text" placeholder="enter room name"/>
+        <input type="text" placeholder="join room"/>
       </div>
       <ul>
         { ["lobby"].map((room, i) => <li key={i}>{room}</li>) }
